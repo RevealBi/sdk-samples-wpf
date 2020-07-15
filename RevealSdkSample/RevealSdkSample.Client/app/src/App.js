985782dcbd0c7fc68254ad3d435b51896d488eeb
Konstantin Koynov <kkoynov@infragistics.com>
2020-07-15 21:39:27 +0300

    Adding React SDK sample.

---
+++ RevealSdkSample/RevealSdkSample.Client/app/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <reveal-view dashboard-name="Sales" show-filters="true"></reveal-view>;
+}
+
+export default App;
---
+++ RevealSdkSample/RevealSdkSample.Server/RevealSdk/RevealSdkContext.cs
@@ -0,0 +1,44 @@
+﻿using Infragistics.Sdk;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace RevealSdkSample.Server.RevealSdk
+{
+    public class RevealSdkContext : IRevealSdkContext
+    {
+        public IRVDataSourceProvider DataSourceProvider => null;
+
+        public IRVDataProvider DataProvider => null;
+
+        public IRVAuthenticationProvider AuthenticationProvider => null;
+
+        public async Task<Stream> GetDashboardAsync(string dashboardId)
+        {
+            return await Task.Run(() =>
+            {
+                var fileName = dashboardId.Split('|')[0];
+                var dashboardFileName = fileName + ".rdash";
+                var resourceName = $"RevealSdkSample.Server.Dashboards.{dashboardFileName}";
+
+                var assembly = Assembly.GetExecutingAssembly();
+                var a = assembly.GetManifestResourceNames();
+                return assembly.GetManifestResourceStream(resourceName);
+            });
+        }
+
+        public async Task SaveDashboardAsync(string userId, string dashboardId, Stream dashboardStream)
+        {
+            await Task.Run(() => { });
+            //var dashboardFileName = dashboardId + ".rdash";
+            //var resourceName = $"Reveal.WebSdk.Sample.Core461.Dashboards.{dashboardFileName}";
+
+            //var assembly = Assembly.GetExecutingAssembly();
+            //var a = assembly.GetManifestResourceNames();
+            //return assembly.GetManifestResourceStream(resourceName);
+        }
+    }
+}
