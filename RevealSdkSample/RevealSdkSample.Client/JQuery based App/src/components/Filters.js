9d1fb20ba265fd5441ed473a6ad4c77f84839a6d
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2020-07-21 14:45:03 +0300

    Filters refactor

--- RevealSdkSample/RevealSdkSample.Client/JQuery based App/src/components/Filters.js
+++ RevealSdkSample/RevealSdkSample.Client/JQuery based App/src/components/Filters.js
@@ -1,124 +1,126 @@
-import React, { Component } from "react";
-import $ from "jquery";
+import React, { useState, useRef, useEffect } from "react";
+const $ = window.$;
 
-export class Filters extends Component {
-  // static setState = Filters.name;
 
-  constructor(props) {
-    super(props);
-    this.state = { selectedOption: "All" };
-    this.handleDataPointClicked = this.handleDataPointClicked.bind(this);
-    this.handleAmericas = this.handleAmericas.bind(this);
-    this.handleOptionChange = this.handleOptionChange.bind(this);
+const CountryChoice = (props) => {
+  return (
+    <>
+      <label className="radioLabels">
+        <input
+          type="radio"
+          name={props.name}
+          value={props.value}
+          checked={props.checked}
+          onChange={() => props.handleChange(props.value)}
+        />
+        {props.value}
+      </label>
+    </>
+  );
+};
+
+
+
+const CountryForm = (props) => {
+  const [selected, setSelected] = useState("All");
+
+  function handleChange(value) {
+    setSelected(value);
+    props.handleOptionChange(value);
   }
 
-  componentDidMount() {
-    this.rv = new $.ig.RevealView("revealView");
-    console.log(this.nv);
+  const data = [
+    { id: 0, value: "All" },
+    { id: 1, value: "Americas" },
+    { id: 2, value: "APAC" },
+    { id: 3, value: "EMEA" },
+    { id: 4, value: "India" },
+    { id: 5, value: "Japan" },
+  ];
 
-    this.rv.addEventListener(
-      "onVisualizationDataPointClicked",
-      this.handleDataPointClicked
-    );
+  const items = data.map(each =>
+    <CountryChoice
+      name="counties"
+      value={each.value}
+      handleChange={handleChange}
+      key={each.id}
+      checked={each.value === selected}
+    />
+  );
+
+  return (
+    <form>
+      {items}
+    </form>
+  );
+};
+
+export const Filters = (props) => {
+  const [_, setOption] = useState('All');
+  const revealEl = useRef(null);
+
+  function handleOptionChange(value) {
+    setOption(value);
   }
 
-  componentWillUnmount() {
-    // this.rv.removeEventListener(
-    //   "onVisualizationDataPointClicked",
-    //   this.handleDataPointClicked
-    // );
-  }
-
-  handleDataPointClicked = (event) => {
+  function handleClick(event) {
     console.log(event);
-  };
-
-  handleAmericas = (event) => {
-    //this.rv.dashboard.filters[0];
-    this.rv.setFilterSelectedValues(this.rv.dashboard.filters()[0], [
-      "Americas",
-    ]);
-    console.log(this.rv.dashboard.filters()[0]);
-  };
-
-  handleOptionChange = (event) => {
-    this.setState({
-      selectedOption: event.target.value,
-    });
-
-    let selectedFilterValues =
-      event.target.value === "All" ? [] : [event.target.value];
-
-    this.rv.setFilterSelectedValues(
-      this.rv.dashboard.filters()[0],
-      selectedFilterValues
-    );
-  };
-
-  render() {
-    return (
-      <div>
-        <span>
-          <form>
-            <label className="radioLabels">
-              <input
-                type="radio"
-                value="All"
-                checked={this.state.selectedOption === "All"}
-                onChange={this.handleOptionChange}
-              />
-              All
-            </label>
-
-            <label className="radioLabels">
-              <input
-                type="radio"
-                value="Americas"
-                checked={this.state.selectedOption === "Americas"}
-                onChange={this.handleOptionChange}
-              />
-              Americas
-            </label>
-            <label className="radioLabels">
-              <input
-                type="radio"
-                value="APAC"
-                checked={this.state.selectedOption === "APAC"}
-                onChange={this.handleOptionChange}
-              />
-              APAC
-            </label>
-            <label className="radioLabels">
-              <input
-                type="radio"
-                value="EMEA"
-                checked={this.state.selectedOption === "EMEA"}
-                onChange={this.handleOptionChange}
-              />
-              EMEA
-            </label>
-            <label className="radioLabels">
-              <input
-                type="radio"
-                value="India"
-                checked={this.state.selectedOption === "India"}
-                onChange={this.handleOptionChange}
-              />
-              India
-            </label>
-            <label className="radioLabels">
-              <input
-                type="radio"
-                value="Japan"
-                checked={this.state.selectedOption === "Japan"}
-                onChange={this.handleOptionChange}
-              />
-              Japan
-            </label>
-          </form>
-        </span>
-        <div id="revealView" />
-      </div>
-    );
   }
-}
+
+  function handleAmericas() {
+    // TODO: port filters logic
+  }
+
+  // Setup reveal dashboard
+  const initRevealBoard = () => {
+    const dashboardId = "Sales";
+    const settings = new $.ig.RevealSettings(dashboardId);
+    settings.showFilters = true;
+
+    $.ig.RevealUtility.loadDashboard(dashboardId, (dashboard) => {
+      settings.dashboard = dashboard;
+      new $.ig.RevealView("#sales", settings);
+    }, (error) => console.log(error));
+  };
+
+  // Setup event listeners and create dashboard
+  useEffect(() => {
+    initRevealBoard();
+    const el = revealEl.current
+    el.addEventListener('onVisualizationDataPointClicked', handleClick);
+    return () => {
+      el.removeEventListener('onVisualizationDataPointClicked', handleClick);
+    };
+  });
+
+  return (
+    <div>
+      <CountryForm handleOptionChange={handleOptionChange} />
+      <div id="sales" ref={revealEl} />
+    </div>
+  );
+};
+
+
+
+//   handleAmericas = (event) => {
+//     //this.rv.dashboard.filters[0];
+//     this.rv.setFilterSelectedValues(this.rv.dashboard.filters()[0], [
+//       "Americas",
+//     ]);
+//     console.log(this.rv.dashboard.filters()[0]);
+//   };
+
+//   handleOptionChange = (event) => {
+//     this.setState({
+//       selectedOption: event.target.value,
+//     });
+
+//     let selectedFilterValues =
+//       event.target.value === "All" ? [] : [event.target.value];
+
+//     this.rv.setFilterSelectedValues(
+//       this.rv.dashboard.filters()[0],
+//       selectedFilterValues
+//     );
+//   };
