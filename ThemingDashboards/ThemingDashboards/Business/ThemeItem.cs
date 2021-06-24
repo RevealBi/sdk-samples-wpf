cef4b85805752cd1ec0a457f6e8fe38a3038d97b
Brian Lagunas <brianlagunas@users.noreply.github.com>
2021-06-24 13:49:18 -0600

    added theming sample

---
+++ ThemingDashboards/ThemingDashboards/Business/ThemeItem.cs
@@ -0,0 +1,45 @@
+﻿using Reveal.Sdk;
+using System.Collections.Generic;
+
+namespace ThemingDashboards.Business
+{
+    public class ThemeItem
+    {
+        public string Name { get; set; }
+
+        public RevealTheme Theme { get; set; }
+
+        public static IEnumerable<ThemeItem> GenerateThemeItems()
+        {
+            List<ThemeItem> themes = new List<ThemeItem>();
+
+            themes.Add(new ThemeItem() { Name = "Mountain Light", Theme = new MountainLightTheme() });
+            themes.Add(new ThemeItem() { Name = "Mountain Dark", Theme = new MountainDarkTheme() });
+            themes.Add(new ThemeItem() { Name = "Ocean Light", Theme = new OceanLightTheme() });
+            themes.Add(new ThemeItem() { Name = "Ocean Dark", Theme = new OceanDarkTheme() });
+            themes.Add(new ThemeItem() { Name = "Custom", Theme = null });
+            return themes;
+        }
+
+        public static RevealTheme CloneTheme(RevealTheme theme)
+        {
+            var newTheme = new RevealTheme();
+            newTheme.AccentColor = theme.AccentColor;
+            newTheme.BoldFont = theme.BoldFont;
+            newTheme.ChartColors.Clear();
+            newTheme.ChartColors.AddRange(theme.ChartColors);
+            newTheme.ConditionalFormatting.MIDColor = theme.ConditionalFormatting.MIDColor;
+            newTheme.ConditionalFormatting.HIColor = theme.ConditionalFormatting.HIColor;
+            newTheme.ConditionalFormatting.LOWColor = theme.ConditionalFormatting.LOWColor;
+            newTheme.ConditionalFormatting.NONEColor = theme.ConditionalFormatting.NONEColor;
+            newTheme.DashboardBackgroundColor = theme.DashboardBackgroundColor;
+            newTheme.FontColor = theme.FontColor;
+            newTheme.HighlightColor = theme.HighlightColor;
+            newTheme.MediumFont = theme.MediumFont;
+            newTheme.RegularFont = theme.RegularFont;
+            newTheme.UseRoundedCorners = theme.UseRoundedCorners;
+            newTheme.VisualizationBackgroundColor = theme.VisualizationBackgroundColor;
+            return newTheme;
+        }
+    }
+}
---
+++ ThemingDashboards/ThemingDashboards/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Reveal.Sdk;
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using ThemingDashboards.Business;
+
+namespace ThemingDashboards
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _themes.ItemsSource = ThemeItem.GenerateThemeItems();
+            _revealView.Dashboard = new RVDashboard(Path.Combine(Environment.CurrentDirectory, "Dashboards/Sales.rdash"));
+        }
+
+        private void Themes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems[0] is ThemeItem themeItem)
+            {
+                RevealTheme theme = RevealSdkSettings.Theme;
+
+                //if the previous and current theme are the same, don't reapply the theme
+                if (themeItem.Theme?.ToString() == theme.ToString())
+                    return;
+
+                if (themeItem.Name == "Custom")
+                {
+                    theme = CreateCustomUglyTheme();
+                }
+                else
+                {
+                    theme = themeItem.Theme;
+                }
+
+                RevealSdkSettings.Theme = theme;
+                _revealView.RefreshTheme();
+            }
+        }
+
+        RevealTheme CreateCustomUglyTheme()
+        {
+            var customTheme = RevealSdkSettings.Theme.Clone();
+
+            var regularFont = new FontFamily(new Uri("pack://application:,,,/Fonts/"), "./#Wingdings 2");
+            var mediumFont = new FontFamily(new Uri("pack://application:,,,/Fonts/"), "./#Lucida Calligraphy");
+            var boldFont = new FontFamily(new Uri("pack://application:,,,/Fonts/"), "./#CF Crack and"); 
+
+            customTheme.RegularFont = regularFont;
+            customTheme.MediumFont = mediumFont;
+            customTheme.BoldFont = boldFont;
+
+            customTheme.FontColor = Color.FromRgb(31, 59, 84);
+            customTheme.AccentColor = Color.FromRgb(192, 80, 77);
+            customTheme.DashboardBackgroundColor = Color.FromRgb(0, 0, 0);
+            customTheme.VisualizationBackgroundColor = Color.FromRgb(153, 255, 255);
+
+            customTheme.ChartColors.Clear();
+            customTheme.ChartColors.Add(Color.FromRgb(192, 80, 77));
+            customTheme.ChartColors.Add(Color.FromRgb(101, 197, 235));
+            customTheme.ChartColors.Add(Color.FromRgb(232, 77, 137));
+
+            return customTheme;
+        }
+    }
+}
