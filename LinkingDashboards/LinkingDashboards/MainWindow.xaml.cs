e89ee9b03b8ab69edb2944a6e13121c4211e8bb8
Brian Lagunas <brianlagunas@users.noreply.github.com>
2021-06-25 11:13:36 -0600

    added linking dashboards sampe

---
+++ LinkingDashboards/LinkingDashboards/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Reveal.Sdk;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace LinkingDashboards
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var filePath = Path.Combine(Environment.CurrentDirectory, "Dashboards/Marketing.rdash");
+            _revealView.Dashboard = new RVDashboard(filePath);
+        }
+
+        private void RevealView_VisualizationLinkingDashboard(object sender, VisualizationLinkingDashboardEventArgs e)
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, "Dashboards/Campaigns.rdash");
+            using (var stream = File.OpenRead(path))
+            {
+                e.Callback("Campaigns", stream);
+            }
+        }
+    }
+}
