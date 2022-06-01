8b60559cfcf1a0baa5581cadafce8dcc4e166865
Brian Lagunas <brianlagunas@users.noreply.github.com>
2022-06-01 15:58:15 -0600

    added rest service samples

---
+++ AddingDataSources/RestService/DataSource-Rest/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Reveal.Sdk;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace DataSource_Rest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _revealView.DataSourcesRequested += RevealView_DataSourcesRequested;
+        }
+
+        private void RevealView_DataSourcesRequested(object sender, DataSourcesRequestedEventArgs e)
+        {
+            var dataSources = new List<RVDashboardDataSource>();
+            var items = new List<RVDataSourceItem>();
+
+            var restDataSource = new RVRESTDataSource();
+            restDataSource.Id = "RestDataSource";
+            restDataSource.Url = "https://excel2json.io/api/share/6e0f06b3-72d3-4fec-7984-08da43f56bb9";
+            restDataSource.Title = "Sales by Category";
+            restDataSource.Subtitle = "Excel2Json";
+            restDataSource.UseAnonymousAuthentication = true;
+            dataSources.Add(restDataSource);
+
+            e.Callback(new RevealDataSources(dataSources, items, false));
+        }
+    }
+}
