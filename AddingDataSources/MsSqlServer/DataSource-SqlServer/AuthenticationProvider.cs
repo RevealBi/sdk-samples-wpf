3b414c485bd25a8ac4e7625dd15ff53f33bedc08
Brian Lagunas <brianlagunas@users.noreply.github.com>
2022-12-09 13:02:46 -0700

    added sql server sample

---
+++ AddingDataSources/MsSqlServer/DataSource-SqlServer/AuthenticationProvider.cs
@@ -0,0 +1,18 @@
+﻿using Reveal.Sdk;
+using System.Threading.Tasks;
+
+namespace DataSource_SqlServer
+{
+    public class AuthenticationProvider : IRVAuthenticationProvider
+    {
+        public Task<IRVDataSourceCredential> ResolveCredentialsAsync(RVDashboardDataSource dataSource)
+        {
+            IRVDataSourceCredential userCredential = new RVUsernamePasswordDataSourceCredential();
+            if (dataSource is RVSqlServerDataSource)
+            {
+                userCredential = new RVUsernamePasswordDataSourceCredential("username", "password");
+            }
+            return Task.FromResult(userCredential);
+        }
+    }
+}
---
+++ AddingDataSources/MsSqlServer/DataSource-SqlServer/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Reveal.Sdk;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace DataSource_SqlServer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            RevealSdkSettings.AuthenticationProvider = new AuthenticationProvider();
+
+            InitializeComponent();
+            _revealView.DataSourcesRequested += RevealView_DataSourcesRequested;
+        }
+
+        private void RevealView_DataSourcesRequested(object sender, Reveal.Sdk.DataSourcesRequestedEventArgs e)
+        {
+            var dataSources = new List<RVDashboardDataSource>();
+            var items = new List<RVDataSourceItem>();
+
+            var sqlDataSource = new RVSqlServerDataSource()
+            {
+                Host = "your-db-host",
+                Database = "your-db-name",
+                Port = 1234,
+                Title = "My SQL Server",
+            };
+            dataSources.Add(sqlDataSource);
+
+            e.Callback(new RevealDataSources(dataSources, null, false));
+        }
+    }
+}
